c84e0acb97dca427a1e0a43b04c22aad5a39ecf5
Anoop C S <anoopcs@redhat.com>
2016-04-26 12:52:27 +0530

    Check for system Gnulib headers during bootstrapping
    Signed-off-by: Anoop C S <anoopcs@redhat.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-git submodule update --init
-./gnulib/gnulib-tool --import human
+# Make use of already present Gnulib headers, if any.
+if [ -f "`command -v gnulib-tool`" ]; then
+        echo "Found Gnulib headers, continuing . . ."
+        gnulib-tool --import human
+else
+        echo "Unable to find Gnulib headers. Preparing Gnulib submodule . . ."
+        git submodule update --init
+        ./gnulib/gnulib-tool --import human
+fi
