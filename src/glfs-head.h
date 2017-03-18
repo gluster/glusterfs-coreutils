5ea9438af103e5a137333f6d207331d0231bd856
ParadoxZero <sidhin.thomas@gmail.com>
2017-03-19 03:22:02 +0530

    prototype head

---
+++ src/glfs-head.h
@@ -0,0 +1,13 @@
+//
+// Created by Sidhin Thomas on 19/03/17.
+//
+
+#ifndef GLUSTERFS_COREUTILS_GLFS_HEAD_H
+#define GLUSTERFS_COREUTILS_GLFS_HEAD_H
+
+#include "glfs-cli.h"
+
+int
+do_head (struct cli_context *ctx);
+
+#endif //GLUSTERFS_COREUTILS_GLFS_HEAD_H
