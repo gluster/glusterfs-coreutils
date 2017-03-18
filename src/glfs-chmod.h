99c7a4e18c7ca29fecb385bea5d1466262f51689
ParadoxZero <sidhin.thomas@gmail.com>
2017-03-19 05:11:48 +0530

    prototype chmod

---
+++ src/glfs-chmod.h
@@ -0,0 +1,11 @@
+//
+// Created by Sidhin Thomas on 19/03/17.
+//
+
+#ifndef GLUSTERFS_COREUTILS_GLFS_CHMOD_H
+#define GLUSTERFS_COREUTILS_GLFS_CHMOD_H
+#include "glfs-cli.h"
+
+int
+do_chmod (struct cli_context *ctx);
+#endif //GLUSTERFS_COREUTILS_GLFS_CHMOD_H
